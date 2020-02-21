7e5bf6da81c463de292c3fe4da17e21974468e12
Piotr Kielak <p.kielak@outlook.com>
2020-02-21 09:07:06 +0100

    event listener fixed

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,13 @@
 const element = document.getElementById('aboutUs');
 const elementHeight = element.offsetHeight;
 const elementOffsetTop = element.offsetTop;
+const windowHeight = window.innerHeight;
 
 let last_known_scroll_position = 0;
 let ticking = false;
 
 function doSomething(scroll_pos) {
-    if(scroll_pos>500){
+    if(scroll_pos>element.offsetHeight/2 + element.offsetTop - windowHeight ){
         console.log('Mam Cię!!!');
     }
 }
