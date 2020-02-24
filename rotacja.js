18fa2426bf5dfd87cff8c3c4ed7e9f36a16716a5
paula-dm <paulina.ziomkowska@gmail.com>
2020-02-24 16:06:03 +0100

    index i rotacja zmienione

--- rotacja.js
+++ rotacja.js
@@ -1,16 +1,16 @@
 const leftArrow = document.getElementById("leftArrow");
 const rightArrow = document.getElementById("rightArrow");
 const heroSection = document.getElementById("hero");
-const heroTitle = document.querySelector(".hero__title");
+const heroTitle = document.querySelector(".hero__header");
 
 let imageCounter = 1;
 const changeHeroImage = function () {
-    imageCounter < 3 ? imageCounter++ : imageCounter = 1;
+    imageCounter < 4 ? imageCounter++ : imageCounter = 1;
     heroSection.classList = `hero hero--image${imageCounter}`;
 };
 
 const changeHeroImageToPrevious = function () {
-    imageCounter === 1 ? imageCounter = 3 : imageCounter--;
+    imageCounter === 1 ? imageCounter = 4 : imageCounter--;
     heroSection.classList = `hero hero--image${imageCounter}`;
 };
 
@@ -18,7 +18,7 @@ let autoImageChanging;
 
 const startAutoImageChanging = function () {
     clearInterval(autoImageChanging);
-    autoImageChanging = setInterval(changeHeroImage, 3000);
+    autoImageChanging = setInterval(changeHeroImage, 2000);
 };
 
 const stopAutoImageChanging = function () {
@@ -33,24 +33,3 @@ rightArrow.addEventListener('click', changeHeroImage);
 
 startAutoImageChanging();
 
-
-// window.addEventListener('load', (event) => {
- 
-// });
-// let slideIndex = 1;
-// showDivs(slideIndex);
-
-// function plusDivs(n) {
-//   showDivs(slideIndex += n);
-// }
-
-// function showDivs(n) {
-//   let i;
-//   let x = document.getElementsByClassName("mySlides");
-//   if (n > x.length) {slideIndex = 1}    
-//   if (n < 1) {slideIndex = x.length} ;
-//   for (i = 0; i < x.length; i++) {
-//      x[i].style.display = "none";  
-//   }
-//   x[slideIndex-1].style.display = "block";  
-// }
