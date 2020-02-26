5b009bb40b406b861566f887742d83f88f88c7f5
Paweł Politowski <matelly@poczta.onet.pl>
2020-02-26 16:30:31 +0100

    Fix

--- script.js
+++ script.js
@@ -38,7 +38,7 @@ document.addEventListener('DOMContentLoaded', function () {
         window.onscroll = function() {
           const scrollPosition = document.documentElement.scrollTop || document.body.scrollTop;
           for (let i in sections) {
-            if (sections[i] <= scrollPosition + 330) {
+            if (sections[i] <= scrollPosition + 200) {
               document.querySelector('.active').setAttribute('class', 'nav');
               document.querySelector('a[href*=' + i + ']').setAttribute('class', 'active');
             }
