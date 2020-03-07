d6626bc01055ce3c9e98fdb4643a08ce22fa6675
paula-dm <paulina.ziomkowska@gmail.com>
2020-03-07 23:01:44 +0100

    wszystko dziala

--- calculator.js
+++ calculator.js
@@ -1,101 +1,45 @@
-let slider = document.getElementById("myRange");
-let output = document.getElementById("people");
-output.innerHTML = slider.value;
-let cena = document.getElementById("cena");
-let show;
-let five = document.getElementById("discount5");
-let fifty = document.getElementById("discount50");
-let newCenka = document.getElementById("cena");
-let newCenusia = document.getElementById("cena");
-let podwojnaCena = document.getElementById("cena");
-let both = document.getElementById("both");
+let slider = document.getElementById("myRange"); //getting the number of peeps
+let people = document.getElementById("people"); // 
+updatePeeps(); //gets number of peeps from slider value
+let totalPrice = document.getElementById("cena"); // Tekst z info o ceną
+let fifty = document.getElementById("discount50"); // checkbox1
+let five = document.getElementById("discount5");   // checkbox2
 
-slider.addEventListener ( "change", function () {
-console.log("input")
-  output.innerHTML = slider.value;
-  let show;
-  
-  if (slider.value == 0) {
-    show = "Proszę wybrać liczbę abonentów";
-  }
-  else if (slider.value == 1 ) {
-    show = "39.99 zł";
-  }
-  else if (slider.value == 2) {
-    show = "49.99 zł";
-  }
-  else if (slider.value == 3) {
-    show = "59.99 zł";
-  }
-  else if (slider.value == 4) {
-    show = "59.99 zł";
-  }
-  else show = "Nie ma takiej opcji."
-  cena.innerHTML = `${show}`;
-  
-   
+function getTotalPrice() { //getting price from
+
+    let basePrices = [0, 39.99, 49.99, 59.99, 59.99];
+    let basePrice = basePrices[slider.value];
+    let discount = 0;
+    if (fifty.checked === true) {
+        discount += (basePrice * 0.5);
+    }
+    if (five.checked === true) {
+        discount += (basePrice * 0.05);
+    }
+    return basePrice - discount;
 }
-);
-five.addEventListener ('click',() => {
 
-  console.log("click")
+function updatePrice () {
+    totalPrice.innerHTML = `${(getTotalPrice()).toFixed(2)} zł`;
+}
 
-  let newCena;
-  if (slider.value == 1)  {
-    
-    newCena = "nowa cena 5% mniej, czyli 37.99 zł";
-  }
-  else if (slider.value == 2) {
-   newCena = "nowa cena 5% mniej, czyli 47.49 zł";
-  }
-  else if (slider.value == 3 || slider.value == 4 ) {
-        
-    newCena = "nowa cena 5% mniej, czyli 56.99 zł";
-  }
-    newCenka.innerHTML = `${newCena}`;
-    console.log(newCena);
-    
-});
+function updatePeeps (){
+     people.innerHTML = slider.value;
+    }
 
-fifty.addEventListener ('click',() => {
 
-  console.log("click")
+slider.addEventListener("change", function () {
   
- let cenka;
-  if (slider.value == 1)  {
-    
-    cenka = "Nowa cena 50% mniej, czyli 19.99 zł";
-  }
-  else if (slider.value == 2) {
-    cenka = "Nowa cena 50% mniej, czyli 24.99 zł";
-  }
-  else if (slider.value == 3 || slider.value == 4 ) {
-        
-    cenka = "Nowa cena 50% mniej, czyli 29.99 zł";
-  }
-    newCenusia.innerHTML = `${cenka}`;
-    console.log(cenka);
- 
+    updatePrice();
+    updatePeeps();
 });
 
-both.addEventListener ('click',() => {
-
-  console.log("click")
-
-  let dwieCeny;
-  if (slider.value == 1)  {
-    
-    dwieCeny = "Nowa cena 55% mniej, czyli 17.99 zł";
-  }
-  else if (slider.value == 2) {
-    dwieCeny = "Nowa cena 55% mniej, czyli 22.49 zł";
-  }
-  else if (slider.value == 3 || slider.value == 4 ) {
-        
-    dwieCeny = "Nowa cena 55% mniej, czyli 26.99 zł";
-  }
-    podwojnaCena.innerHTML = `${dwieCeny}`;
-    console.log(podwojnaCena);
- 
+fifty.addEventListener("change", () => {
+  
+    updatePrice();
+   
 });
 
+five.addEventListener("change", () => {
+    updatePrice();
+});
